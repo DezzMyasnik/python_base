bf352222258255a237a28b46470526fedebdb0d4
mini <dezz1985@gmail.com>
2019-06-25 22:09:52 +0300

    нарисована дугобразная радуга

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -13,19 +13,19 @@ start_point = sd.get_point(50, 50)
 end_point = sd.get_point(350, 450)
 width = 50
 step = 50
-i = 1
-for color in rainbow_colors:
-    new_point_sart =sd.get_point(start_point.x + i*step, start_point.y)
-    new_point_end = sd.get_point(end_point.x + i * step, end_point.y)
-    sd.line(new_point_sart, new_point_end, color, width)
-    i += 1
+#i = 1
+#for color in rainbow_colors:
+##    new_point_sart = sd.get_point(start_point.x + i*step, start_point.y)
+#    new_point_end = sd.get_point(end_point.x + i * step, end_point.y)
+#    sd.line(new_point_sart, new_point_end, color, width)
+#    i += 1
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
-#i = 1
-#for color in rainbow_colors:
-#    point=sd.get_point(420, -50)
-#    sd.circle(point, 300 + i * step, color, width)
-#    i += 1
+
+i = 1
+for color in rainbow_colors:
+    point=sd.get_point(420, -50)
+    sd.circle(point, 300 + i * step, color, width)
+    i += 1
 sd.pause()
