77185aa0d6f37dc7dbc4e11f115f9a9a8413b3a0
mini <dezz1985@gmail.com>
2019-08-12 14:55:42 +0300

    вывел информацию по жителям района

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,7 +6,18 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from district.central_street.house1 import room1 as room11, room2 as room22
+from district.central_street.house2 import room1, room2
+#from district.soviet_street import house1, house2
 
+#first = room1.folks
+#second = room2.folks
+#print(second+first)
+#print(room2.folks.extend(room1.folks))
+print("На районе живут: {}".format(", ".join(room11.folks +
+                                            room22.folks +
+                                            room1.folks +
+                                            room2.folks)))
 
 
 
