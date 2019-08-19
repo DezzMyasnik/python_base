14bc8221525105acbc285b99a496b172d72624aa
mini <dezz1985@gmail.com>
2019-08-19 17:45:35 +0300

    добавил советкую улицу. развел по улицам

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -8,14 +8,27 @@
 
 from district.central_street.house1 import room1 as room11, room2 as room22
 from district.central_street.house2 import room1, room2  # TODO забыли с советской улице
-#from district.soviet_street import house1, house2
+from district.soviet_street.house1 import room1 as s_room1, room2 as s_room2
+from district.soviet_street.house2 import room1 as s_room12, room2 as s_room22
 
-#first = room1.folks
-#second = room2.folks
-#print(second+first)
-#print(room2.folks.extend(room1.folks))
-print("На районе живут: {}".format(", ".join(room11.folks +
+#
+print("На районе живут: {}".format(", \n".join(room11.folks +
                                             room22.folks +
                                             room1.folks +
-                                            room2.folks)))
+                                            room2.folks +
+                                            s_room1.folks +
+                                            s_room2.folks +
+                                            s_room12.folks +
+                                            s_room22.folks)))
 
+
+print("На Центральной улице живут: {}".format(", ".join(room11.folks +
+                                            room22.folks +
+                                            room1.folks +
+                                            room2.folks
+                                            )))
+print("На Советской улице живут: {}".format(", ".join(s_room1.folks +
+                                            s_room2.folks +
+                                            s_room12.folks +
+                                            s_room22.folks
+                                            )))
