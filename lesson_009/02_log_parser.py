86bb26f2dccdb325924935dfc70a7c72cd4de776
mini <dezz1985@gmail.com>
2019-10-17 23:59:43 +0300

    сделал парсер с группировкой по минутам

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,7 +19,58 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+
+import datetime
+from time import strftime
+from pprint import pprint
+
+
+class Parser:
+
+    def __init__(self, filename):
+        self.name = filename
+        self.log_val = {}
+        self.log = []
+        self.stata = {}
+
+    def read_log(self):
+        with open(self.name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self.log_parese(line)
+                # self.collect(line)
+            self.nok_count()
+
+    def log_parese(self, line):
+        line = line.rstrip('\n')
+        splitted = line.rsplit('] ')
+        splitted[0] = splitted[0].lstrip('[')
+        # print(splitted[0].find('.', 0, len(splitted[0])))
+        splitted[0] = splitted[0][:splitted[0].find('.', 0, len(splitted[0])) - 3]
+        local = [splitted[1], splitted[0]]
+        self.log.append(local)
+
+    def nok_count(self):
+        for item in self.log:
+
+            if item[0] == 'NOK':
+
+                if item[1] in self.stata:
+
+                    self.stata[item[1]] += 1
+                else:
+                    self.stata[item[1]] = 1
+
+    def __str__(self):
+
+        return str(list(self.stata))
+
+
+test = Parser('events.txt')
+test.read_log()
+with open('events_res.txt', 'w') as file:
+    for item in test.stata.items():
+        str_out = '[{}] {}\n'.format(item[0], item[1])
+        file.write(str_out)
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
