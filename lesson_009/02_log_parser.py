c69c5563371fbfbf2b0cbf77c83cb761dc45fe04
mini <dezz1985@gmail.com>
2019-10-18 07:18:03 +0300

    сделал парсер с группировкой по часам, дням, месяцам и годам

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,17 +19,13 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-
-import datetime
-from time import strftime
-from pprint import pprint
+from abc import ABCMeta, abstractmethod
 
 
-class Parser:
+class Parser(metaclass=ABCMeta):
 
     def __init__(self, filename):
         self.name = filename
-        self.log_val = {}
         self.log = []
         self.stata = {}
 
@@ -37,38 +33,85 @@ class Parser:
         with open(self.name, 'r', encoding='cp1251') as file:
             for line in file:
                 self.log_parese(line)
-                # self.collect(line)
+
             self.nok_count()
+
+    @abstractmethod
+    def _get_data_str(self,data):
+        pass
+
 
     def log_parese(self, line):
         line = line.rstrip('\n')
         splitted = line.rsplit('] ')
         splitted[0] = splitted[0].lstrip('[')
-        # print(splitted[0].find('.', 0, len(splitted[0])))
-        splitted[0] = splitted[0][:splitted[0].find('.', 0, len(splitted[0])) - 3]
+        splitted[0] = self._get_data_str(splitted[0])
         local = [splitted[1], splitted[0]]
         self.log.append(local)
 
     def nok_count(self):
         for item in self.log:
-
             if item[0] == 'NOK':
-
                 if item[1] in self.stata:
-
                     self.stata[item[1]] += 1
                 else:
                     self.stata[item[1]] = 1
 
     def __str__(self):
+        return str(self.stata)
 
-        return str(list(self.stata))
+class MinutPareser(Parser):
+
+    def _get_data_str(self,data):
+        data = data[:data.find('.', 0, len(data)) - 3]
+        return data
+
+class HourPareser(Parser):
+
+    def _get_data_str(self,data):
+        data = data[:data.find('.', 0, len(data)) - 6]
+        return data
+
+class DayPareser(Parser):
+
+    def _get_data_str(self,data):
+        data = data[:data.find('.', 0, len(data)) - 9]
+        return data
+
+class MounthParser(Parser):
+
+    def _get_data_str(self,data):
+        data = data[:data.find('.', 0, len(data)) - 12]
+        return data
 
 
-test = Parser('events.txt')
+class YaerParser(Parser):
+
+    def _get_data_str(self, data):
+        data = data[:data.find('.', 0, len(data)) - 15]
+        return data
+
+
+out_list =[]
+test = MinutPareser('events.txt')
 test.read_log()
+out_list.extend(test.stata.items())
+test = HourPareser('events.txt')
+test.read_log()
+out_list.extend(test.stata.items())
+test = DayPareser('events.txt')
+test.read_log()
+out_list.extend(test.stata.items())
+test = MounthParser('events.txt')
+test.read_log()
+out_list.extend(test.stata.items())
+test = YaerParser('events.txt')
+test.read_log()
+out_list.extend(test.stata.items())
+
+
 with open('events_res.txt', 'w') as file:
-    for item in test.stata.items():
+    for item in out_list:
         str_out = '[{}] {}\n'.format(item[0], item[1])
         file.write(str_out)
 
