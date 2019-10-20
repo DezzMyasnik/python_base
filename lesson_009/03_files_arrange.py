475c4248820ca1d04924969b7ffb7163afa90ac1
mini <dezz1985@gmail.com>
2019-10-21 01:31:17 +0300

    сделана сортировка по дате создания файлов

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -33,8 +33,53 @@ import os, time, shutil
 #
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+class Sorter:
 
-# TODO здесь ваш код
+    def __init__(self,inputDir,outDir):
+        inputDir_normalized = os.path.normpath(inputDir)
+        outDir_normalized = os.path.normpath(outDir)
+        self.inputDir = inputDir_normalized
+        self.outDir = outDir_normalized
+
+    def get_files(self):
+        for dirpath, dirnames, filenames in os.walk(self.inputDir):
+
+            for file in filenames:
+                full_file_path = os.path.join(dirpath, file)
+                file_time = self.get_file_attrebutes(full_file_path)
+                self.make_dir(file_time)
+                self.copy_files(full_file_path)
+                #print(full_file_path, secs, file_time)
+                #if file_time[0] == 2013:
+                    # выводим только файлы за 2013 год
+
+    def make_dir(self, file_time):
+        if os.path.exists(os.path.join(self.outDir, str(file_time.tm_year))):
+            if not os.path.exists(os.path.join(self.outDir,str(file_time.tm_year), str(file_time.tm_mon))):
+                os.makedirs(os.path.join(self.outDir,str(file_time.tm_year), str(file_time.tm_mon)))
+
+        else:
+            os.makedirs(os.path.join(self.outDir, str(file_time.tm_year)))
+            self.make_dir(file_time)
+
+
+    def get_file_attrebutes(self,full_file_path):
+        secs = os.path.getmtime(full_file_path)
+        return time.gmtime(secs)
+
+    def copy_files(self, filename):
+        dirname, fname= os.path.split(filename)
+        file_time = self.get_file_attrebutes(filename)
+        outputName = os.path.join(self.outDir,str(file_time.tm_year), str(file_time.tm_mon), fname)
+        shutil.copy2(filename,outputName)
+
+
+
+inputDir = os.path.join(os.path.dirname(__file__),'icons')
+outDir  = os.path.join(os.path.dirname(__file__),'icons_by_year')
+test = Sorter(inputDir, outDir )
+
+test.get_files()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
