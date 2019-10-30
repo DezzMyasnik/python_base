4bf5230035c3074153f5dfcb1aef2b1ba29782c5
mini <dezz1985@gmail.com>
2019-10-31 01:14:18 +0300

    сделано..

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -17,8 +17,70 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
+
+from numpy import random
+
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+    pass
+
+class CarCrashError(Exception):
+    pass
+
+class GluttonyError(Exception):
+    pass
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+class DrunkError(Exception):
+    pass
+
+
+
+
+
+def one_day():
+
+    try:
+         ob = random.choice([IamGodError('Я есть божество'),
+                             DrunkError("Напился"),
+                             CarCrashError("Разбил машину"),
+                             GluttonyError("Обожрался"),
+                             DepressionError("Стрессанул"),
+                             SuicideError("Самоубился"), BaseException()],
+                             size=1,
+                             p=[1/13,1/13,1/13, 1/13, 1/13, 1/13,7/13])
+
+         raise ob[0]
+    except (IamGodError, DrunkError, CarCrashError,GluttonyError,DepressionError,SuicideError, BaseException) as exc:
+        with open('log.txt', 'a', encoding='utf-8') as file:
+            if type(exc) is not BaseException:
+                file.writelines(f'Поймано исключение {exc} \n')
+            else:
+                file.writelines(f'Поймано базовое исключение \n')
+
+
+    return random.randint(1, 7)
+"""
+        raise IamGodError('Я есть божество')
+        raise DrunkError("Напился")
+        raise CarCrashError("Разбил машину")
+        raise GluttonyError("Обожрался")
+        raise DepressionError ("Стрессанул")
+        raise SuicideError("Самоубился")"""
+
+
+
+i=0
+while i <= ENLIGHTENMENT_CARMA_LEVEL:
+    i += one_day()
+    print(i)
 
 # https://goo.gl/JnsDqu
